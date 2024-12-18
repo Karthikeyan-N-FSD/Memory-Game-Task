0da5ff1e2cbdd9d6a8085298677b19b3d4573636
Karthikeyan-N-FSD <karthikeyan89@gmail.com>
2024-12-18 23:09:02 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,74 @@
+const gameBoard = document.getElementById('game-board');
+        const cards = ["A", "A", "B", "B", "C", "C", "D", "D", "E", "E", "F", "F", "G", "G", "H", "H"];
+        let firstCard = null;
+        let secondCard = null;
+
+        function shuffle(array) {
+            for (let i = array.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [array[i], array[j]] = [array[j], array[i]];
+
+            }
+        }
+
+        function createBoard() {
+            shuffle(cards);
+            gameBoard.innerHTML = '';
+            cards.forEach(card => {
+                const cardElement = document.createElement('div');
+                cardElement.classList.add('card');
+                cardElement.dataset.value = card;
+                cardElement.innerHTML = `
+                    <div class="front" style="background-image: url('./Images/${card}.jpg');"></div>
+                    <div class="back"></div>
+                `;
+                cardElement.addEventListener('click', flipCard);
+                gameBoard.appendChild(cardElement);
+            });
+        }
+
+        function flipCard() {
+            if (this === firstCard) return;
+            this.classList.add('flipped');
+            if (!firstCard) {
+                firstCard = this;
+                return;
+            }
+
+            secondCard = this;
+            checkMatch();
+        }
+
+        function checkMatch() {
+            const isMatch = firstCard.dataset.value === secondCard.dataset.value;
+            if (isMatch) {
+                disableCards();
+            } else {
+                unflipCards();
+            }
+        }
+
+        function disableCards() {
+            firstCard.removeEventListener('click', flipCard);
+            secondCard.removeEventListener('click', flipCard);
+            resetBoard();
+        }
+
+        function unflipCards() {
+            setTimeout(() => {
+                firstCard.classList.remove('flipped');
+                secondCard.classList.remove('flipped');
+                resetBoard();
+            }, 1000);
+        }
+
+        function resetBoard() {
+            [firstCard, secondCard] = [null, null];
+        }
+
+        function restartGame() {
+            resetBoard();
+            createBoard();
+        }
+
+        createBoard();

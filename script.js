8af2517546ef7db9b3040f339b16e3390943a4b0
Karthikeyan-N-FSD <karthikeyan89@gmail.com>
2024-12-18 23:30:49 +0530

    added lockBoard var

--- script.js
+++ script.js
@@ -1,74 +1,76 @@
 const gameBoard = document.getElementById('game-board');
-        const cards = ["A", "A", "B", "B", "C", "C", "D", "D", "E", "E", "F", "F", "G", "G", "H", "H"];
-        let firstCard = null;
-        let secondCard = null;
+const cards = ["A", "A", "B", "B", "C", "C", "D", "D", "E", "E", "F", "F", "G", "G", "H", "H"];
+let firstCard = null;
+let secondCard = null;
+let lockBoard = false;
 
-        function shuffle(array) {
-            for (let i = array.length - 1; i > 0; i--) {
-                const j = Math.floor(Math.random() * (i + 1));
-                [array[i], array[j]] = [array[j], array[i]];
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
 
-            }
-        }
+    }
+}
 
-        function createBoard() {
-            shuffle(cards);
-            gameBoard.innerHTML = '';
-            cards.forEach(card => {
-                const cardElement = document.createElement('div');
-                cardElement.classList.add('card');
-                cardElement.dataset.value = card;
-                cardElement.innerHTML = `
+function createBoard() {
+    shuffle(cards);
+    gameBoard.innerHTML = '';
+    cards.forEach(card => {
+        const cardElement = document.createElement('div');
+        cardElement.classList.add('card');
+        cardElement.dataset.value = card;
+        cardElement.innerHTML = `
                     <div class="front" style="background-image: url('./Images/${card}.jpg');"></div>
                     <div class="back"></div>
                 `;
-                cardElement.addEventListener('click', flipCard);
-                gameBoard.appendChild(cardElement);
-            });
-        }
+        cardElement.addEventListener('click', flipCard);
+        gameBoard.appendChild(cardElement);
+    });
+}
 
-        function flipCard() {
-            if (this === firstCard) return;
-            this.classList.add('flipped');
-            if (!firstCard) {
-                firstCard = this;
-                return;
-            }
+function flipCard() {
+    if (lockBoard || this === firstCard) return;
+    this.classList.add('flipped');
+    if (!firstCard) {
+        firstCard = this;
+        return;
+    }
 
-            secondCard = this;
-            checkMatch();
-        }
+    secondCard = this;
+    checkMatch();
+}
 
-        function checkMatch() {
-            const isMatch = firstCard.dataset.value === secondCard.dataset.value;
-            if (isMatch) {
-                disableCards();
-            } else {
-                unflipCards();
-            }
-        }
+function checkMatch() {
+    const isMatch = firstCard.dataset.value === secondCard.dataset.value;
+    if (isMatch) {
+        disableCards();
+    } else {
+        unflipCards();
+    }
+}
 
-        function disableCards() {
-            firstCard.removeEventListener('click', flipCard);
-            secondCard.removeEventListener('click', flipCard);
-            resetBoard();
-        }
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    resetBoard();
+}
 
-        function unflipCards() {
-            setTimeout(() => {
-                firstCard.classList.remove('flipped');
-                secondCard.classList.remove('flipped');
-                resetBoard();
-            }, 1000);
-        }
+function unflipCards() {
+    lockBoard = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flipped');
+        secondCard.classList.remove('flipped');
+        resetBoard();
+    }, 1000);
+}
 
-        function resetBoard() {
-            [firstCard, secondCard] = [null, null];
-        }
+function resetBoard() {
+    [firstCard, secondCard, lockBoard] = [null, null, false];
+}
 
-        function restartGame() {
-            resetBoard();
-            createBoard();
-        }
+function restartGame() {
+    resetBoard();
+    createBoard();
+}
 
-        createBoard();
+createBoard();
